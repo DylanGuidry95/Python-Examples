95c98363dd45e3ca9350286962fa698dbc4b4dd1
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-03-13 15:20:28 -0500

    Added Ellipse Arc and Text drawing

--- AStarDrawing/main.py
+++ AStarDrawing/main.py
@@ -1,16 +1,15 @@
+''''''
 from Utils import *
 import sys
 
 size = width, height = 1600, 900
 screen = pygame.display.set_mode(size)
 
-rect = Rectangle(screen, [10, 10], WHITE, [30, 30], True)
-circle = Circle(screen, [1600, 900], GOLD, 10)
-line = Line(screen, [[250, 20], [350, 500]], PURPLE, 10)
+pygame.init()
 
-rect.draw()
-circle.draw()
-line.draw()
+text = Text(screen, [100,100], [WHITE, BLACK], "Dick Butt", 15)
+
+text.draw()
 
 while 1:
     for event in pygame.event.get():
@@ -19,4 +18,5 @@ while 1:
 
     pygame.display.flip()
     pygame.display.flip()
+
 
