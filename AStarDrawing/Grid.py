2353369b633fc0bc0c24604cc41dc13081f046da
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-03-14 12:57:46 -0500

    Begin game logic

--- AStarDrawing/Grid.py
+++ AStarDrawing/Grid.py
@@ -3,11 +3,23 @@ from Utils import*
 size = width, height = 1600, 900
 screen = pygame.display.set_mode(size)
 
+middleclick = False
+rightclick = False
+leftclick = False
+
+mtimer = 0
+rtimer = 0
+ltimer = 0
+
+deltatime = 0.0
+lastTick = 0.0
+
 class Graph(object):
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.nodes = []
+        lastTick = 0.0
 
     def setupgraph(self):
         for x in range(0, self.width):
@@ -17,8 +29,54 @@ class Graph(object):
     def drawgraph(self):
         for node in self.nodes:
             node.drawvisual()
-            
-class NodeVisiual(object):
+
+class AStar(object):
+    def __init__(self, graph):
+        self.graph = graph
+        self.start = None
+        self.goal = None
+
+    def setstartnode(self, node):
+        if node in self.graph.nodes:
+            if self.start == None:
+                self.start = node
+            else:
+                self.start.changestate("start")
+                self.start = node
+                self.start.changestate("start")
+
+    def setgoalnode(self, node):
+        if node in self.graph.nodes:
+            if self.start == None:
+                self.start = node
+            else:
+                self.start.changestate("goal")
+                self.start = node
+                self.start.changestate("goal")
+
+    def modifywall(self, node):
+        if node in self.graph.nodes:
+            node.changestate("wall")
+
+    def update(self):               
+        timer = pygame.time.get_ticks()
+        deltatime = (timer - lastTick)
+        lastTick = timer 
+
+        for node in self.graph.nodes:
+            mxpos, mypos = pygame.mouse.get_pos()
+            if pygame.mouse.get_pressed()[1] and not middleclick:
+                middleclick = True
+                if node.visual.collisioncheck([mxpos, mypos]):
+                    self.modifywall(node)        
+        if middleclick == True:
+            mtimer = mtimer + (deltatime)
+        if mtimer > 400:
+            mtimer = 0
+            middleclick = False
+
+
+class NodeObject(object):
     def __init__(self, position, color):
         self.position = position
         self.visual = Rectangle(screen, [position[0], position[1]], color, [25, 25], 30)
@@ -31,17 +89,27 @@ class NodeVisiual(object):
 
     def drawtonode(self, node):
         circle = Circle(screen, [self.position[0] + (25/2), self.position[1] + (25/2)],
-                             BLACK, 10)
+                        BLACK, 10)
         line = Line(screen, [[self.position[0] + (25/2), self.position[1] + (25/2)],
-                             [node.nodevisual.visual.drawposition[0] + (25/2), 
-                             node.nodevisual.visual.drawposition[1] + (25/2)]], BLACK, 2)
+                             [node.nodevisual.visual.drawposition[0] + (25/2),
+                              node.nodevisual.visual.drawposition[1] + (25/2)]], BLACK, 2)
         circle.draw()
         line.draw()
+
+    def drawposition(self):
+        return self.visual.drawposition
+
+    def collisioncheck(self, colpos):
+        if colpos[0] > self.drawposition()[0] and colpos[0] < self.drawposition()[0] + 25:
+            if colpos[1] > self.drawposition()[1] and colpos[1] < self.drawposition()[1] + 25:
+                return True
+        else:
+            return False
 
 class Node(object):
     def __init__(self, position):
         self.position = position
-        self.nodevisual = NodeVisiual(self.position, WHITE)
+        self.visual = NodeObject(self.position, WHITE)
         self.gscore = 0
         self.hscore = 0
         self.fscore = 0
@@ -50,33 +118,30 @@ class Node(object):
         self.isStart = False
 
     def drawvisual(self):
-        self.nodevisual.drawNode()
+        self.visual.drawNode()
 
     def drawconnection(self, node):
-        self.nodevisual.drawtonode(node)
-    
+        self.visual.drawtonode(node)
 
-    def clicked(self, state):
+    def changestate(self, state):
         if state == "start":
             self.isStart = not self.isStart
             if self.isStart:
-                self.nodevisual.changecolor(GREEN)
+                self.visual.changecolor(GREEN)
             else:
-                self.nodevisual.changecolor(WHITE)
+                self.visual.changecolor(WHITE)
         elif state == "goal":
             self.isGoal = not self.isGoal
             if self.isGoal:
-                self.nodevisual.changecolor(BLUE)
+                self.visual.changecolor(BLUE)
             else:
-                self.nodevisual.changecolor(WHITE)
-            
+                self.visual.changecolor(WHITE)
         elif state == "wall":
             self.isWall = not self.isWall
             if self.isWall:
-                self.nodevisual.changecolor(RED)
+                self.visual.changecolor(RED)
             else:
-                self.nodevisual.changecolor(WHITE)
-
+                self.visual.changecolor(WHITE)
 
 class NodeInformation(object):
     def __init__(self, position):
@@ -86,9 +151,9 @@ class NodeInformation(object):
         gposition = self.position
         hposition = [self.position[0], self.position[1] + 50]
         fposition = [self.position[0], self.position[1] + 100]
-        g = Text(screen, gposition, [WHITE, BLACK], "G Score: " + str(node.position[0]), 25)
-        h = Text(screen, hposition, [WHITE, BLACK], "H Score:" + str(node.position[1]), 25)
-        f = Text(screen, fposition, [WHITE, BLACK], "F Score:" + str(node.fscore), 25)
-        g.draw()
-        h.draw()
-        f.draw()
+        gtext = Text(screen, gposition, [WHITE, BLACK], "G Score: " + str(node.position[0]), 25)
+        htext = Text(screen, hposition, [WHITE, BLACK], "H Score:" + str(node.position[1]), 25)
+        ftext = Text(screen, fposition, [WHITE, BLACK], "F Score:" + str(node.fscore), 25)
+        gtext.draw()
+        htext.draw()
+        ftext.draw()
--- AStarDrawing/main.py
+++ AStarDrawing/main.py
@@ -8,6 +8,8 @@ pygame.init()
 
 grid = Graph(53, 25)
 
+algorithm = AStar(grid)
+
 grid.setupgraph()
 
 info = NodeInformation([0, 750])
@@ -15,70 +17,17 @@ info.drawinformation(grid.nodes[0])
 
 pygame.event.get()
 
-middleclick = False
-rightclick = False
-leftclick = False
-
-mtimer = 0
-rtimer = 0
-ltimer = 0
-
-deltatime = 0.0
-lastTick = 0.0
 
 while 1:    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-    timer = pygame.time.get_ticks()
-    deltatime = (timer - lastTick)
-    lastTick = timer
+
 
     grid.drawgraph()
 
-    if pygame.mouse.get_pressed()[1] and middleclick == False:
-        middleclick = True
-        for node in grid.nodes:    
-            x, y = pygame.mouse.get_pos()
-            if x > node.nodevisual.visual.drawposition[0] and x < node.nodevisual.visual.drawposition[0] + 25:
-                if y > node.nodevisual.visual.drawposition[1] and y < node.nodevisual.visual.drawposition[1] + 25:
-                    node.clicked("wall")
-                    info.drawinformation(node)
-    if pygame.mouse.get_pressed()[0] and rightclick == False:
-        rightclick = True
-        for node in grid.nodes:    
-            x, y = pygame.mouse.get_pos()
-            if x > node.nodevisual.visual.drawposition[0] and x < node.nodevisual.visual.drawposition[0] + 25:
-                if y > node.nodevisual.visual.drawposition[1] and y < node.nodevisual.visual.drawposition[1] + 25:
-                    node.clicked("start")
-                    info.drawinformation(node)
-    if pygame.mouse.get_pressed()[2] and leftclick == False:
-        leftclick = True
-        for node in grid.nodes:    
-            x, y = pygame.mouse.get_pos()
-            if x > node.nodevisual.visual.drawposition[0] and x < node.nodevisual.visual.drawposition[0] + 25:
-                if y > node.nodevisual.visual.drawposition[1] and y < node.nodevisual.visual.drawposition[1] + 25:                
-                    node.clicked("goal")
-                    info.drawinformation(node)
-
-    if rightclick == True:
-        rtimer = rtimer + (deltatime)
-        if rtimer > 400:
-            rtimer = 0
-            rightclick = False
-    
-    if middleclick == True:
-        mtimer = mtimer + (deltatime)
-        if mtimer > 400:
-            mtimer = 0
-            middleclick = False
-    
-    if leftclick == True:
-        ltimer = ltimer + (deltatime)
-        if ltimer > 400:
-            ltimer = 0
-            leftclick = False
+    algorithm.update()
 
 
 
