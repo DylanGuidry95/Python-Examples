aacf72a1323b05644844b70348810617a28a0920
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-03-14 10:42:50 -0500

    draw lines from node to node

--- AStarDrawing/Grid.py
+++ AStarDrawing/Grid.py
@@ -18,17 +18,40 @@ class Graph(object):
         for node in self.nodes:
             node.drawvisual()
             
+class NodeVisiual(object):
+    def __init__(self, position, color):
+        self.position = position
+        self.visual = Rectangle(screen, [position[0], position[1]], color, [25, 25], 30)
+
+    def drawNode(self):
+        self.visual.draw()
+
+    def changecolor(self, color):
+        self.visual.changecolor(color)
+
+    def drawtonode(self, node):
+        circle = Circle(screen, [self.position[0] + (25/2), self.position[1] + (25/2)],
+                             BLACK, 10)
+        line = Line(screen, [[self.position[0] + (25/2), self.position[1] + (25/2)],
+                             [node.nodevisual.visual.drawposition[0] + (25/2), 
+                             node.nodevisual.visual.drawposition[1] + (25/2)]], BLACK, 2)
+        circle.draw()
+        line.draw()
 
 class Node(object):
     def __init__(self, position):
         self.position = position
-        self.nodevisual = Rectangle(screen, [position[0], position[1]], WHITE, [25, 25], 30)
+        self.nodevisual = NodeVisiual(self.position, WHITE)
         self.gscore = 0
         self.hscore = 0
-        self.fscore = 0    
+        self.fscore = 0
 
     def drawvisual(self):
-        self.nodevisual.draw()
+        self.nodevisual.drawNode()
+
+    def drawconnection(self, node):
+        self.nodevisual.drawtonode(node)
+    
 
     def clicked(self):
         self.nodevisual.changecolor(RED)
--- AStarDrawing/main.py
+++ AStarDrawing/main.py
@@ -21,12 +21,13 @@ while 1:
 
     grid.drawgraph()
 
-    
+    grid.nodes[0].drawconnection(grid.nodes[26])
+
     for node in grid.nodes:
         if pygame.mouse.get_pressed()[1]:
             x, y = pygame.mouse.get_pos()
-            if x > node.nodevisual.drawposition[0] and x < node.nodevisual.drawposition[0] + 25:
-                if y > node.nodevisual.drawposition[1] and y < node.nodevisual.drawposition[1] + 25:
+            if x > node.nodevisual.visual.drawposition[0] and x < node.nodevisual.visual.drawposition[0] + 25:
+                if y > node.nodevisual.visual.drawposition[1] and y < node.nodevisual.visual.drawposition[1] + 25:
                     node.clicked()
                     info.drawinformation(node)
 
