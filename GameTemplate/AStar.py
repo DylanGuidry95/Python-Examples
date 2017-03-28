a552c76f2239f72952ebfc3c06aa30d973bab7b1
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-03-28 16:19:54 -0500

    Pre test commit

--- GameTemplate/AStar.py
+++ GameTemplate/AStar.py
@@ -12,7 +12,7 @@ class Graph(object):
         nodecount = 0
         for row in range(0, self.width):
             for col in range(0, self.height):
-                self.nodes.append(Node(Vector2(row, col), nodecount))
+                self.nodes.append(Node(Vector2(col, row), nodecount))
                 nodecount = nodecount + 1
 
     def getnode(self, nodeval):
@@ -29,6 +29,7 @@ class Node(object):
         self.position = position
         self.parent = None
         self.value = value
+        self.iswalkable = True
 
     def getneighbors(self, graph):
         '''Gets all the neighbors of this node in the graph passed in'''
@@ -48,14 +49,14 @@ class Node(object):
                     neighbors.append(node)
         return neighbors
 
-    def calcgscore(self, node):
+    def calcgscore(self, node, isinlist):
         '''Calculates the gscore for the node based on the position of the node passed in'''
         tentativeg = 0
         if self.position.xpos == node.position.xpos or self.position.ypos == node.position.ypos:
             tentativeg = 10
         else:
             tentativeg = 14
-        if self.parent is None:
+        if not isinlist:
             self.gscore = node.gscore + tentativeg
             self.parent = node
         else:
@@ -99,37 +100,56 @@ class AStarAlgorithm(object):
                     node = compare
                     compare = temp
 
-    def retrace(self):    
+    def retrace(self):
         current = self.endnode
-        while current.parent is not None:
+        path = []
+        while current is not None:
+            path.append(current)
             current = current.parent
+        return path
 
     def algorithm(self):
         if self.begnode is None or self.endnode is None:
             return "No Start or Goal set"
         current = self.begnode
         self.openlist.append(current)
-        while self.endnode not in self.closelist:
-            neighbors = current.getneighbors(self.graph)
-            for node in neighbors:
-                if node not in self.openlist or node not in self.closelist:
-                    self.openlist.append(node)
-                    node.calcgscore(current)
-                    node.calchscore(self.endnode)
-                    node.calcfscore()
-            self.sortopenlist()
-            current = self.openlist[0]
+        while len(self.openlist) > 0:
             self.openlist.remove(current)
             self.closelist.append(current)
-
-        if self.endnode in self.closelist:
-            return self.retrace()
+            neighbors = current.getneighbors(self.graph)
+            for node in neighbors:
+                if node not in self.openlist:
+                    if node not in self.closelist:
+                        if node.iswalkable:
+                            self.openlist.append(node)
+                            node.calcgscore(current, False)
+                else:
+                    if node not in self.closelist:
+                        node.calcgscore(current, True)
+                node.calchscore(self.endnode)
+                node.calcfscore()
+            self.openlist.sort(key=lambda node: node.fscore)
+            current = self.openlist[0]
+            if self.endnode in self.closelist:
+                return self.retrace()
+        return None
 
 
 graph = Graph(10, 10)
 graph.gengraph()
 algo = AStarAlgorithm(graph)
-algo.setgoal(graph.nodes[len(graph.nodes) - 1])
-algo.setstart(graph.nodes[0])
+algo.setgoal(graph.getnode(47))
+algo.setstart(graph.getnode(43))
+graph.getnode(35).iswalkable = False
+graph.getnode(45).iswalkable = False
+graph.getnode(55).iswalkable = False
+
+bad = []
+for node in graph.nodes:
+    if not node.iswalkable:
+        bad.append(node)
+
 
 test = algo.algorithm()
+for node in test:
+    print node.position
